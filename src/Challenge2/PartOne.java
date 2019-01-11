bfa590e80c124389bbdef5043a168dadc7135d14
pmaldera <pierremaldera@hotmail.fr>
2019-01-11 19:12:14 +0100

    Challenge 2

--- src/Challenge2/PartOne.java
+++ src/Challenge2/PartOne.java
@@ -5,33 +5,38 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Stack;
 
 public class PartOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File file = new File("./inputs/input1.txt");
-		int sum = 0;
+		File file = new File("./inputs/input2.txt");
 		int length;
-		char[] chars = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		char[] chars = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+				'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+		int[] count = new int[] { 0, 0 };
+		Stack<Integer> stack;
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-		    String line;
-		    HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-			for(char c : chars)
-			{
-				map.put(c, 0);
-			}
-		    while ((line = br.readLine()) != null) {
-		    	length = line.length();
-		       for(char c : chars)
-				{
-					map.put(c, (line.length() - length));
-					line.replace(c+"", "");
-
+			String line;
+			while ((line = br.readLine()) != null) {
+				stack = new Stack<Integer>();
+				length = line.length();
+				for (char c : chars) {
+					line = line.replace(c + "", "");
+					int amount = (length - line.length());
+					length = line.length();
+					if (amount >= 2 && amount <= 3 && stack.search(amount) == -1)
+						stack.push(amount);
 				}
-		    }
-		    System.out.println(sum);
+				while (stack.empty() != true) {
+					int pop = stack.pop();
+					count[pop - 2]++;
+				}
+
+			}
+			System.out.println(count[0] * count[1]);
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/Challenge2/PartTwo.java
+++ src/Challenge2/PartTwo.java
@@ -11,35 +11,42 @@ public class PartTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File file = new File("./inputs/input1.txt");
-		int sum = 0;
-		boolean found = false;
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		while (found == false) {
-			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-				String line;
-				while ((line = br.readLine()) != null) {
-					sum += Integer.parseInt(line);
-					if(list.contains(sum))
-					{
-						found = true;
+		File file = new File("./inputs/input2.txt");
+		ArrayList<String> list = new ArrayList<String>();
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				list.add(line);
+			}
+
+			for (int i = 0; i < list.size() - 1; i++) {
+				for (int j = i + 1; j < list.size() - 1; j++) {
+					String l1 = list.get(i);
+					String l2 = list.get(j);
+					StringBuilder l3 = new StringBuilder(l2);
+					String l4;
+					for (int x = 0; x < l1.length(); x++) {
+						if (l1.charAt(x) == l2.charAt(x)) {
+							l3.setCharAt(x, '0');
+						}
+					}
+
+					l4 = l3.toString().replace("0", "");
+					if (l4.length() == 1) {
+
+						System.out.println(l2.replace(l4, ""));
 						break;
 					}
-					else
-					{
-						list.add(sum);
-					}
 				}
-				
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		System.out.println(sum);
 	}
 
 }
